0e1610fbe8a6629b08cf4703cab57775aea4306f
Zhou Xianhua <xianhua.zhou@gmail.com>
2012-06-04 21:58:20 +0800

    added option --trim-bundlename (-t)

--- Command/GenerateAssetsVersionsCommand.php
+++ Command/GenerateAssetsVersionsCommand.php
@@ -11,19 +11,32 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand,
 
 /**
  * generate asset version number wth git log
- * Usage: app/console mdpi:assets:versions <BUNDLE NAME> 
+ * Usage: app/console mdpi:assets:versions [BUNDLE NAME]
  *
  * e.g. 
- *    app/console -e=prod mdpi:assets:versions MDPIMain 
+ *    # generate all of assets from all of bundles
+ *    app/console -e=prod mdpi:assets:versions
  *
+ *    # generate assets of MDPIMainBundle
+ *    app/console -e=prod mdpi:assets:versions MDPIMain
+ *
+ *    # generate assets of MDPIMainBundle, and remove "mdpimain" from the assets path
+ *    app/console -e=prod mdpi:assets:versions MDPIMain --trim-bundlename
  */
 class GenerateAssetsVersionsCommand extends ContainerAwareCommand 
 {
     protected function configure()
     {
-        $this->setName("mdpi:assets:versions")
-            ->setDescription("generate version number (git log) for each asset file")
-            ->addArgument("bundle", InputArgument::REQUIRED, "bundle name");
+        $this->setName("mdpi:assets:versions")->
+            setDescription("generate version number (git log) for each asset file")->
+            addArgument("bundle", InputArgument::OPTIONAL, "bundle name, generate only for the specified bundle if set")->
+            addOption(
+                'trim-bundlename', 
+                't', 
+                InputOption::VALUE_OPTIONAL, 
+                'If set, bundle names will be removed from the generated assets path'
+            );
+        
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -33,21 +46,48 @@ class GenerateAssetsVersionsCommand extends ContainerAwareCommand
         $assetsFile = $container->getParameter('kernel.cache_dir') . '/' . 
             AssetsExtension::ASSETS_FILE;
 
-        $webBundleDir = realpath($container->getParameter('kernel.root_dir') . '/../web/bundles/' . $bundleName);
-        if (!is_dir($webBundleDir)) {
-            throw new \Exception('Bundle "' . $bundleName . '" does not exists.');
+        $bundlesDir = realpath(
+            $container->getParameter('kernel.root_dir') . 
+            '/../web/bundles'
+        );
+        $trimBundlename = $input->getOption('trim-bundlename');
+
+        if ($bundleName) {
+            $webBundleDirs = array($bundlesDir . '/' . $bundleName);
+            if (!is_dir($webBundleDirs[0])) {
+                throw new \Exception('Hmmmm, it seems that the bundle "' . 
+                    $bundleName . '" does not exists.');
+            }
+        } else {
+            $webBundleDirs = glob($bundlesDir . '/*');
+            if (count($webBundleDirs) == 0) {
+                throw new \Exception('No bundles in the web directory found? 
+                    Try do "app/console assets:install" first.');
+            }
         }
-        $webBundleDirSize = strlen($webBundleDir);
 
         $fp = fopen($assetsFile, 'w');
         flock($fp, LOCK_EX);
         fwrite($fp, "<?php\nreturn array(");
-        foreach (Finder::create()->files()->name('*')->in($webBundleDir) as $file) {
-            fwrite($fp, '"' . substr($file, $webBundleDirSize + 1) . '" => "' . substr(md5_file($file), 0, 16) . "\",\n");
+
+        foreach ($webBundleDirs as $webBundleDir) {
+            $webBundleDirSize = $trimBundlename ? 
+                strlen($webBundleDir) :
+                strlen(dirname($webBundleDir));
+
+            foreach (Finder::create()->files()->name('*')->in($webBundleDir) as $file) {
+                fwrite($fp, 
+                    '"' . substr($file, $webBundleDirSize + 1) . 
+                    '" => "' . 
+                    substr(md5_file($file), 0, 16) . "\",\n"
+                );
+            }
         }
+
         fwrite($fp, ");");
         flock($fp, LOCK_UN);
         fclose($fp);
-    }
 
+        $output->writeln('Cool, an assets file is generated: ' . $assetsFile);
+    }
 }
